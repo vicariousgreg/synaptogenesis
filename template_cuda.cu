212f5888778692ef2df8ce036c4008c5783c6a12
Gregory Davis <vicariousgreg@github.com>
2016-10-05 22:07:28 -0400

    simpler template

--- template_cuda.cu
+++ template_cuda.cu
@@ -2,9 +2,18 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-__device__ float div(int x, float f) {
+__device__ float divide(int x, float f) {
     return float(x) / f;
 }
+
+__device__ float (*divide_ptr)(int, float) = divide;
+
+__device__ float mult(int x, float f) {
+    return x * f;
+}
+
+__device__ float (*mult_ptr)(int, float) = mult;
+
 
 template <typename OUT, typename... ARGS>
 __global__ void sum(float(*func)(OUT, ARGS...), OUT* outputs, int size, float* dest, ARGS... args) {
@@ -13,12 +22,6 @@ __global__ void sum(float(*func)(OUT, ARGS...), OUT* outputs, int size, float* d
         s += (*func)(outputs[i], args...);
     }
     *dest = s;
-    //*dest = 0.0;
-}
-
-template <typename... Types>
-__global__ void setup_kernel(float (**my_callback)(Types...)){
-  *my_callback = &div;
 }
 
 void check_error() {
@@ -32,26 +35,30 @@ void check_error() {
 }
 
 int main(void) {
+    // Set up testing data
     int vals[] = { 1, 2, 3 };
     int* device_vals;
     cudaMalloc(&device_vals, 3 * sizeof(int));
     cudaMemcpy(device_vals, vals, 3 * sizeof(int), cudaMemcpyHostToDevice);
 
+    // Set up space for the result
+    float local_answer = 0.0;
+    float *device_answer;
+    cudaMalloc(&device_answer, sizeof(float));
+
+    // DIVIDE
     float(*local_div)(int, float);
-    float(**device_div)(int, float);
-    cudaMalloc(&device_div, sizeof(void *));
-
-    setup_kernel<<<1, 1>>>((float(**)(int, float))device_div);
-    cudaMemcpy((void *)&local_div, (void *)device_div, sizeof(void *), cudaMemcpyDeviceToHost);
-
-    float *answer;
-    cudaMalloc(&answer, sizeof(float));
-
-    sum<<<1, 1>>>(local_div, device_vals, 3, answer, (float) 2.0);
+    cudaMemcpyFromSymbol(&local_div, divide_ptr, sizeof(void *));
+    sum<int, float><<<1, 1>>>(local_div, device_vals, 3, device_answer, (float) 2.0);
+    cudaMemcpy(&local_answer, device_answer, sizeof(float), cudaMemcpyDeviceToHost);
     check_error();
+    std::cout << local_answer << "\n";
 
-    float local_answer = 1.0;
-    cudaMemcpy(&local_answer, answer, sizeof(float), cudaMemcpyDeviceToHost);
-
-    std::cout << local_answer;
+    // MULT
+    float(*local_mult)(int, float);
+    cudaMemcpyFromSymbol(&local_mult, mult_ptr, sizeof(void *));
+    sum<int, float><<<1, 1>>>(local_mult, device_vals, 3, device_answer, (float) 2.0);
+    cudaMemcpy(&local_answer, device_answer, sizeof(float), cudaMemcpyDeviceToHost);
+    check_error();
+    std::cout << local_answer << "\n";
 }
