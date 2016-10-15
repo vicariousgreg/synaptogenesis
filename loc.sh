7e90f4a862bc10718b2655d4428ee64e564853db
Gregory Davis <vicariousgreg@github.com>
2016-10-14 21:48:11 -0400

    using already built loc instead of shitty script

--- loc.sh
+++ loc.sh
@@ -1,30 +1,4 @@
 find src | \
   grep "\(\.h\|\.cpp\)" | \
   grep -v "CImg" | 
-  xargs cat | \
-  wc -l | \
-  xargs echo Total lines:
-
-find src | \
-  grep "\(\.h\|\.cpp\)" | \
-  grep -v "CImg" | 
-  xargs cat | \
-  sed '/^\s*$/d' | \
-  wc -l | \
-  xargs echo Non-blank lines:
-
-find src | \
-  grep "\(\.h\|\.cpp\)" | \
-  grep -v "CImg" | 
-  xargs cat | \
-  sed '/^\s*\(#\|\/\/\|\/\*\|\*\)/d;/^\s*$/d' | \
-  wc -l | \
-  xargs echo Lines of code:
-
-find src | \
-  grep "\(\.h\|\.cpp\)" | \
-  grep -v "CImg" | 
-  xargs cat | \
-  sed '/^\s*\(#\|\/\/\|\/\*\|\*\)/!d;/^\s*$/d' | \
-  wc -l | \
-  xargs echo Comments and preprocessor directives: 
+  xargs cloc
