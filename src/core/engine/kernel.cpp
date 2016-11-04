1db732d4c3b48c56dc207c8fb685beda78efd189
Gregory Davis <vicariousgreg@github.com>
2016-11-03 20:18:22 -0400

    combined serial and parallel kernels, placing preprocessor directives inside the functions

--- src/core/engine/kernel.cpp
+++ src/core/engine/kernel.cpp
@@ -70,76 +70,9 @@ GLOBAL void clear_data(float* data, int count) {
         data[nid] = 0.0;
 }
 
-#ifdef PARALLEL
-GLOBAL void calc_fully_connected(Instruction inst) {
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
-
-    if (col < inst.to_size) {
-        float sum = 0;
-        for (int row = 0 ; row < inst.from_size ; ++row) {
-            sum += inst.extractor(inst, inst.outputs[row])
-                * inst.weights[row * inst.to_size + col];
-        }
-        inst.inputs[col] = calc(inst.opcode, inst.inputs[col], sum);
-    }
-}
-
-GLOBAL void calc_one_to_one(Instruction inst) {
-    int index = blockIdx.x * blockDim.x + threadIdx.x;
-
-    if (index < inst.from_size) {
-        inst.inputs[index] = calc(inst.opcode, inst.inputs[index],
-            inst.extractor(inst, inst.outputs[index]) * inst.weights[index]);
-    }
-}
-
-GLOBAL void calc_convergent(Instruction inst) {
-    int d_row = blockIdx.x * blockDim.x + threadIdx.x;
-    int d_col = blockIdx.y * blockDim.y + threadIdx.y;
-    int d_index = d_row*inst.to_columns + d_col;
-
-    if (d_row < inst.to_rows and d_col < inst.to_columns) {
-        float sum = 0.0;
-        int s_row = d_row * inst.stride - inst.fray;
-        int s_col = d_col * inst.stride - inst.fray;
-
-        // Kernels are organized into columns
-        // One kernel per destination neuron
-        //   Unless convolutional (shared kernel)
-        int kernel_size = inst.overlap * inst.overlap;
-        int kernel_row_size = (inst.convolutional)
-                              ? 1 : inst.to_rows * inst.to_columns;
-
-        // Column of matrix is either the first column (convolutional)
-        //   or the index of the destination neuron otherwise
-        int weight_col = (inst.convolutional) ? 0 : d_index;
-
-        // Run the kernel
-        for (int k_row = 0 ; k_row < inst.overlap ; ++k_row) {
-            for (int k_col = 0 ; k_col < inst.overlap ; ++k_col) {
-                int k_s_row = s_row + k_row;
-                int k_s_col = s_col + k_col;
-                // The connection is frayed if the layers are the same size
-                // Avoid making connections with non-existent neurons!
-                if (inst.fray != 0 and (k_s_row < 0 or k_s_row >= inst.to_rows
-                    or k_s_col < 0 or k_s_col >= inst.to_columns))
-                    continue;
-
-                int s_index = k_s_row * inst.from_columns + k_s_col;
-
-                // Row of matrix is the kernel index * row size (see above)
-                int weight_offset =
-                    ((k_row*inst.overlap) + k_col)
-                    * kernel_row_size;
-
-                sum += inst.extractor(inst, inst.outputs[s_index]) *
-                    inst.weights[weight_offset + weight_col];
-            }
-        }
-        inst.inputs[d_index] = calc(inst.opcode, inst.inputs[d_index], sum);
-    }
-}
-#else
+/******************************************************************************/
+/****************************** CONNECTION KERNELS ****************************/
+/******************************************************************************/
 
 /* IMPORTANT:
  * Serial implementation is faster if matrix is interpreted in a transposed
@@ -148,7 +81,18 @@ GLOBAL void calc_convergent(Instruction inst) {
  *    contiguous in memory.
  */
 
-void calc_fully_connected(Instruction inst) {
+GLOBAL void calc_fully_connected(Instruction inst) {
+#ifdef PARALLEL
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    if (col < inst.to_size) {
+        float sum = 0;
+        for (int row = 0 ; row < inst.from_size ; ++row) {
+            sum += inst.extractor(inst, inst.outputs[row])
+                * inst.weights[row * inst.to_size + col];
+        }
+        inst.inputs[col] = calc(inst.opcode, inst.inputs[col], sum);
+    }
+#else
     for (int row = 0 ; row < inst.to_size ; ++row) {
         float sum = 0.0;
         for (int col = 0 ; col < inst.from_size ; ++col) {
@@ -157,38 +101,66 @@ void calc_fully_connected(Instruction inst) {
         }
         inst.inputs[row] = calc(inst.opcode, inst.inputs[row], sum);
     }
+#endif
 }
 
-void calc_one_to_one(Instruction inst) {
+GLOBAL void calc_one_to_one(Instruction inst) {
+#ifdef PARALLEL
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    if (index < inst.from_size) {
+        inst.inputs[index] = calc(inst.opcode, inst.inputs[index],
+            inst.extractor(inst, inst.outputs[index]) * inst.weights[index]);
+    }
+#else
     for (int index = 0 ; index < inst.from_size ; ++index) {
         inst.inputs[index] = calc(inst.opcode, inst.inputs[index],
             inst.extractor(inst, inst.outputs[index]) * inst.weights[index]);
     }
+#endif
 }
 
-void calc_convergent(Instruction inst) {
+GLOBAL void calc_convergent(Instruction inst) {
     int kernel_size = inst.overlap * inst.overlap;
 
+#ifdef PARALLEL
+    int d_row = blockIdx.x * blockDim.x + threadIdx.x;
+    int d_col = blockIdx.y * blockDim.y + threadIdx.y;
+    int d_index = d_row*inst.to_columns + d_col;
+    if (d_row < inst.to_rows and d_col < inst.to_columns) {
+#else
     // Iterate over destination neurons
     for (int d_row = 0 ; d_row < inst.to_rows ; ++d_row) {
         for (int d_col = 0 ; d_col < inst.to_columns ; ++d_col) {
             int d_index = d_row * inst.to_columns + d_col;
+#endif
             float sum = 0.0;
 
             // Determine starting row and column for source neurons
             int s_row = d_row * inst.stride - inst.fray;
             int s_col = d_col * inst.stride - inst.fray;
 
+#ifdef PARALLEL
+            // Column of matrix is either the first column (convolutional)
+            //   or the index of the destination neuron otherwise
+            int weight_col = (inst.convolutional) ? 0 : d_index;
+            // Kernels are organized into columns
+            // One kernel per destination neuron
+            //   Unless convolutional (shared kernel)
+            int kernel_row_size = (inst.convolutional)
+                                  ? 1 : inst.to_rows * inst.to_columns;
+#else
             // Row of matrix is either the first column (convolutional)
             //   or the index of the destination neuron otherwise
             int weight_offset = (inst.convolutional)
                                 ? 0 : d_index * kernel_size;
+#endif
 
             // Run the kernel
             for (int k_row = 0 ; k_row < inst.overlap ; ++k_row) {
                 for (int k_col = 0 ; k_col < inst.overlap ; ++k_col) {
                     int k_s_row = s_row + k_row;
                     int k_s_col = s_col + k_col;
+
                     // The connection is frayed if the layers are the same size
                     // Avoid making connections with non-existent neurons!
                     if (inst.fray != 0 and (k_s_row < 0 or k_s_row >= inst.to_rows
@@ -197,14 +169,24 @@ void calc_convergent(Instruction inst) {
 
                     int s_index = k_s_row * inst.from_columns + k_s_col;
 
+#ifdef PARALLEL
+                    // Row of matrix is the kernel index * row size (see above)
+                    int weight_offset =
+                        ((k_row*inst.overlap) + k_col)
+                        * kernel_row_size;
+#else
                     // Column of matrix is the kernel index
                     int weight_col = (k_row * inst.overlap) + k_col;
+#endif
+
                     sum += inst.extractor(inst, inst.outputs[s_index]) *
                         inst.weights[weight_offset + weight_col];
                 }
             }
             inst.inputs[d_index] = calc(inst.opcode, inst.inputs[d_index], sum);
+#ifdef PARALLEL
+#else
         }
+#endif
     }
 }
-#endif
