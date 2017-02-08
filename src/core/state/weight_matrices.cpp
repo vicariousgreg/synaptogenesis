ac9791aa12380e9d3b590abc281f765cb59b35d1
Gregory Davis <vicariousgreg@github.com>
2017-02-07 21:45:46 -0500

    refactoring weight matrices

--- src/core/state/weight_matrices.cpp
+++ src/core/state/weight_matrices.cpp
@@ -5,159 +5,119 @@
 #include "util/error_manager.h"
 #include "util/parallel.h"
 
-static void initialize_matrix(Connection* conn, float* mData, int weight_depth);
+/* Clears an array */
+static void clear(float* vals, int num_vals) {
+    for (int i = 0 ; i < num_vals ; ++i) vals[i] = 0.0;
+}
 
-WeightMatrices::WeightMatrices(Model *model, int weight_depth) {
-    auto connections = model->get_connections();
+/* Randomizes an array */
+static void randomize(float* vals, int num_vals, float max) {
+    for (int i = 0 ; i < num_vals ; ++i) vals[i] = fRand(0, max);
+}
 
-    // Allocate one big glob for weights
-    // Skip shared weights because they don't take up extra space
-    int total_size = 0;
-    for (auto& conn : connections) {
-        int matrix_size = conn->get_num_weights();
-        // If plastic, multiply by depth to make room.
-        if (conn->plastic)
-            matrix_size *= weight_depth;
+/* Transfers the values from one array to another */
+static void transfer(float* from, float* to, int num_vals) {
+    for (int i = 0 ; i < num_vals ; ++i) to[i] = from[i];
+}
 
-        if (conn->get_parent() == NULL)
-            total_size += matrix_size;
-    }
+/* Initializes weights using connection parameters */
+static void initialize(float* target_matrix, Connection* conn) {
+    std::stringstream stream(conn->get_init_params());
+    int num_weights = conn->get_num_weights();
 
+    // Extract first value
+    float value;
+    stream >> value;
+
+    // If there are no more values, set all weights to first value
+    // Otherwise, read values and initialize
+    if (stream.eof()) {
+        for (int index = 0 ; index < num_weights ; ++index)
+            target_matrix[index] = value;
+    } else {
+        // If parallel, transpose the input...
+        // Parallel convergent matrices are laid out such that each
+        //   kernel is in a column
 #ifdef PARALLEL
-    cudaMalloc((&this->matrix_datas), total_size * sizeof(float));
-    cudaCheckError("Failed to allocate space for weight matrices!");
-#else
-    this->matrix_datas = (float*)malloc(total_size * sizeof(float));
-    if (this->matrix_datas == NULL)
-        ErrorManager::get_instance()->log_error(
-            "Failed to allocate space for weight matrices!");
-#endif
-
-    // Allocate double pointer for indexing purposes
-    float* curr_point = this->matrix_datas;
-    for (auto & conn : connections) {
-        if (conn->get_parent() == NULL) {
-            this->matrices[conn] = curr_point;
-            // Skip over appropriate amount of memory
-            // Plastic matrices might have additional layers
-            if (conn->plastic)
-                curr_point += (conn->get_num_weights() * weight_depth);
-            else
-                curr_point += conn->get_num_weights();
-        } else {
-            this->matrices[conn] = this->matrices[conn->get_parent()];
+        int rows, cols;
+        int field_size = conn->get_field_size();
+        switch (conn->type) {
+            case FULLY_CONNECTED:
+                ErrorManager::get_instance()->log_error(
+                    "Cannot specify all weights for fully connected matrix!");
+            case CONVOLUTIONAL:
+                rows = field_size * field_size;
+                cols = 1;
+                break;
+            case CONVERGENT:
+                rows = field_size * field_size;
+                cols = conn->to_layer->size;
+                break;
+            case ONE_TO_ONE:
+                rows = conn->from_layer->size;
+                cols = conn->to_layer->size;
+                break;
         }
-    }
-
-    // Initialize weights
-    for (auto & conn : connections) {
-        // Skip shared connections
-        if (conn->get_parent() == NULL)
-            initialize_matrix(conn, this->matrices[conn], weight_depth);
-    }
-}
-
-WeightMatrices::~WeightMatrices() {
-#ifdef PARALLEL
-    cudaFree(this->matrix_datas);
+        for (int col = 0 ; col < cols ; ++col) {
+            for (int row = 0 ; row < rows ; ++row) {
+                target_matrix[row * cols + col] = value;
+                if (row != rows-1 and col != cols-1 and stream.eof())
+                    ErrorManager::get_instance()->log_error(
+                        "Insufficient number of weights specified!");
+                stream >> value;
+            }
+        }
 #else
-    free(this->matrix_datas);
+        for (int index = 0 ; index < num_weights ; ++index) {
+            target_matrix[index] = value;
+            if (index != num_weights-1 and stream.eof())
+                ErrorManager::get_instance()->log_error(
+                    "Insufficient number of weights specified!");
+            stream >> value;
+        }
 #endif
+    }
 }
 
-/* Initializes matrix */
-static void initialize_matrix(Connection* conn,
-        float* mData, int weight_depth) {
-    // Multiply by depth if plastic
+WeightMatrix::WeightMatrix(Connection* conn, int matrix_depth) {
     int num_weights = conn->get_num_weights();
     int matrix_size = num_weights;
-    if (conn->plastic) matrix_size *= weight_depth;
+    // Multiply by depth if plastic
+    if (conn->plastic) matrix_size *= matrix_depth;
 
     // Copy data over to a target matrix
     // Parallel requires a temporary matrix be created and copied
     // Serial accesses mData directly
 #ifdef PARALLEL
+    cudaMalloc((&mData), matrix_size * sizeof(float));
+    cudaCheckError("Failed to allocate space for weight matrices!");
+
     float *target_matrix = (float*)calloc(matrix_size, sizeof(float));
     if (target_matrix == NULL)
         ErrorManager::get_instance()->log_error(
             "Failed to allocate temporary matrix on host for randomization!");
 #else
+    mData = (float*)malloc(matrix_size * sizeof(float));
+    if (mData == NULL)
+        ErrorManager::get_instance()->log_error(
+            "Failed to allocate space for weight matrices!");
     float *target_matrix = mData;
 #endif
 
     // If parameter is specified, interpret it for initialization
     // Otherwise, perform randomization
-    auto init_params = conn->get_init_params();
-    if (init_params.size() > 0) {
-        std::stringstream stream(init_params);
-
-        // Extract first value
-        float value;
-        stream >> value;
-
-        // If there are no more values, set all weights to first value
-        // Otherwise, read values and initialize
-        if (stream.eof()) {
-            for (int index = 0 ; index < num_weights ; ++index)
-                target_matrix[index] = value;
-        } else {
-            // If parallel, transpose the input...
-            // Parallel convergent matrices are laid out such that each
-            //   kernel is in a column
-#ifdef PARALLEL
-            int rows, cols;
-            int field_size = conn->get_field_size();
-            switch (conn->type) {
-                case FULLY_CONNECTED:
-                    ErrorManager::get_instance()->log_error(
-                        "Cannot specify all weights for fully connected matrix!");
-                case CONVOLUTIONAL:
-                    rows = field_size * field_size;
-                    cols = 1;
-                    break;
-                case CONVERGENT:
-                    rows = field_size * field_size;
-                    cols = conn->to_layer->size;
-                    break;
-                case ONE_TO_ONE:
-                    rows = conn->from_layer->size;
-                    cols = conn->to_layer->size;
-                    break;
-            }
-            for (int col = 0 ; col < cols ; ++col) {
-                for (int row = 0 ; row < rows ; ++row) {
-                    target_matrix[row * cols + col] = value;
-                    if (row != rows-1 and col != cols-1 and stream.eof())
-                        ErrorManager::get_instance()->log_error(
-                            "Insufficient number of weights specified!");
-                    stream >> value;
-                }
-            }
-#else
-            for (int index = 0 ; index < num_weights ; ++index) {
-                target_matrix[index] = value;
-                if (index != num_weights-1 and stream.eof())
-                    ErrorManager::get_instance()->log_error(
-                        "Insufficient number of weights specified!");
-                stream >> value;
-            }
-#endif
-        }
+    if (conn->get_init_params().size() == 0) {
+        randomize(target_matrix, num_weights, conn->max_weight);
     } else {
-        for (int index = 0 ; index < num_weights ; ++index)
-            target_matrix[index] = fRand(0, conn->max_weight);
+        initialize(target_matrix, conn);
     }
 
     if (conn->plastic) {
-        // Set up baseline weight
-        for (int index = 0 ; index < num_weights ; ++index) {
-            target_matrix[num_weights + index] = target_matrix[index];
-        }
+        float *baseline = target_matrix + num_weights;
+        transfer(target_matrix, baseline, num_weights);
 
-        // Set up further layers if necessary (initialize to zero)
-        for (int index = 2*num_weights ; index < matrix_size ; ++index) {
-            target_matrix[index] = 0.0;
-        }
+        float *trace = target_matrix + 2*num_weights;
+        clear(trace, num_weights);
     }
 
 #ifdef PARALLEL
@@ -166,4 +126,28 @@ static void initialize_matrix(Connection* conn,
     cudaCheckError("Failed to initialize weight matrix!");
     free(target_matrix);
 #endif
+}
+
+WeightMatrix::~WeightMatrix() {
+#ifdef PARALLEL
+    cudaFree(this->mData);
+#else
+    free(this->mData);
+#endif
+}
+
+WeightMatrices::WeightMatrices(Model *model, int matrix_depth) {
+    // Set up non-shared weights first
+    for (auto & conn : model->get_connections())
+        if (conn->get_parent() == NULL)
+            this->matrices[conn] = new WeightMatrix(conn, matrix_depth);
+
+    // Set up shared weights (just duplicate)
+    for (auto & conn : model->get_connections())
+        if (conn->get_parent() != NULL)
+            this->matrices[conn] = this->matrices[conn->get_parent()];
+}
+
+WeightMatrices::~WeightMatrices() {
+    for (auto matrix : this->matrices) delete matrix.second;
 }
--- src/core/state/weight_matrices.h
+++ src/core/state/weight_matrices.h
@@ -4,16 +4,28 @@
 #include "model/model.h"
 #include "util/constants.h"
 
+class WeightMatrix {
+    public:
+        WeightMatrix(Connection *conn, int matrix_depth);
+        virtual ~WeightMatrix();
+
+        float* get_data() const { return mData; }
+
+    private:
+        float *mData;
+};
+
 class WeightMatrices {
     public:
-        WeightMatrices(Model *model, int weight_depth);
+        WeightMatrices(Model *model, int matrix_depth);
         virtual ~WeightMatrices();
 
-        float* get_matrix(Connection* conn) const { return matrices.at(conn); }
+        float* get_matrix(Connection* conn) const {
+            return matrices.at(conn)->get_data();
+        }
 
     protected:
-        float* matrix_datas;
-        std::map<Connection*, float*> matrices;
+        std::map<Connection*, WeightMatrix*> matrices;
 };
 
 #endif
